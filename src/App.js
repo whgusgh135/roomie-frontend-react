9def6ead8aa1d7a80472bf82d4a581bb4beb7c98
Hyunho Cho <whgusgh1357@gmail.com>
2019-01-15 23:47:37 +1300

    add login form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/reducer';
 
 import { Home } from './components/Home/Home';
 import { Navbar } from './components/Navbar/Navbar';
 import { Sidebar } from './components/Sidebar/Sidebar';
+import { LoginForm } from './components/Auth/LoginForm';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-        <div class="container">
-            <Navbar />
-            
-            <Sidebar />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div class="container">
+              <Navbar />
+              
+              <Sidebar />
 
-            <Home />
-        </div>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/auth" component={LoginForm} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
