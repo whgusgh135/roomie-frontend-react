a7ce25c1e9f3ee20d2584874fe2cfa1e83a5d523
Hyunho Cho <whgusgh1357@gmail.com>
2019-01-19 21:38:10 +1300

    update navbar with state

--- src/components/Navbar/NavbarProfile.js
+++ src/components/Navbar/NavbarProfile.js
@@ -5,12 +5,11 @@ import profileImage from '../../styles/img/profile.jpg';
 
 export const NavbarProfile = props => {
     // replace this value with state
-    const userName = "Kevin Cho";
 
     return (
         <a href="#" class="nav-items__profile">
             <img src={profileImage} alt="profile" class="nav-items__profile--img" />
-            <p class="nav-items__profile--name">{userName}</p>
+            <p class="nav-items__profile--name">{props.auth.user.firstName} {props.auth.user.lastName}</p>
         </a>
     )
 }
