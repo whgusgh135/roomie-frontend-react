2c7f061c02fa2519615a599a838e04a6c9b9c5c9
Hyunho Cho <whgusgh1357@gmail.com>
2019-01-20 22:08:46 +1300

    initial implementation of getting image

--- src/components/Navbar/NavbarProfile.js
+++ src/components/Navbar/NavbarProfile.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 // replace this image with state
 import profileImage from '../../styles/img/profile.jpg';
 
-export const NavbarProfile = props => {
-    // replace this value with state
+export class NavbarProfile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profileImage: "/api/profileimage/" + this.props.auth.user.roomie.profileImage
+        }
+    }
 
-    return (
-        <a href="#" class="nav-items__profile">
-            <img src={profileImage} alt="profile" class="nav-items__profile--img" />
-            <p class="nav-items__profile--name">{props.auth.user.firstName} {props.auth.user.lastName}</p>
-        </a>
-    )
+    
+    
+    render() {
+        return (
+            <a href="#" class="nav-items__profile">
+                <img src={this.state.profileImage} alt="profile" class="nav-items__profile--img" />
+                <p class="nav-items__profile--name">{this.props.auth.user.firstName} {this.props.auth.user.lastName}</p>
+            </a>
+        )
+    }
 }
